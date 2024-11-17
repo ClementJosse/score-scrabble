e48aa8ca512f8bc12bf5ab6715ce99b255ab433d
ClementJosse <clement.b.josse@gmail.com>
2024-11-17 17:23:15 +0100

    Base du menu et de l'application avec VueJS

---
+++ src/App.vue
@@ -0,0 +1,55 @@
+<template>
+  <div class="main">
+    <TitleLogo />
+    <button class="new-game">Nouvelle Partie</button>
+  </div>
+</template>
+
+<script>
+import TitleLogo from './components/TitleLogo.vue'; // Assurez-vous que le chemin est correct
+
+export default {
+  name: 'App',
+  components: {
+    TitleLogo
+  }
+}
+</script>
+
+<style>
+.main {
+  display: flex;
+  flex-direction: column; /* Dispose les éléments verticalement */
+  align-items: center; /* Centre les éléments horizontalement */
+  width: clamp(0px, 400px, 80vw);
+  height: 100vh;
+  margin: auto; /* Centre horizontalement la div */
+  margin-top: clamp(0px, 100px, 20vw);
+}
+
+button {
+  margin-top: 20px; /* Ajoute un espace entre le titre et le bouton */
+  color: #ffffff; /* Définit la couleur du texte en vert */
+  background-color: #027A56; /* Définit la couleur du texte en vert */
+  width: 100%;
+  height: clamp(0px, 65px, 13vw);
+  font-size: clamp(0px, 30px, 6vw); /* Ajuste la taille du texte */
+  font-weight: 600;
+  border-radius: clamp(0px, 10px, 2vw);
+  border: 0px;
+  margin-top: clamp(0px, 200px, 40vw);
+  margin-bottom: clamp(0px, 200px, 40vw);
+}
+
+button:active {
+  background-color: #006345; /* Couleur lorsqu'on appuie */
+}
+
+#app {
+  font-family: Helvetica, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+}
+</style>
---
+++ src/components/TitleLogo.vue
@@ -0,0 +1,36 @@
+<template>
+  <div class="TitleLogo">
+    <img src="@/assets/logo.png" alt="Logo" />
+    <span class="title-text">ScoreScrabble</span>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'TitleLogo',
+}
+</script>
+
+<style scoped>
+.TitleLogo {
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  max-height: fit-content;
+  width: 100%; /* S'assure que la div prend tout l'espace disponible */
+}
+
+.TitleLogo img {
+  flex: 1; /* Prend 1/4 de l'espace */
+  max-width: 25%; /* Limite la largeur à 25% de la div */
+  height: auto;
+}
+
+.TitleLogo .title-text {
+  flex: 3; /* Prend 3/4 de l'espace */
+  color: #027A56; /* Définit la couleur du texte en vert */
+  font-size: clamp(0px, 40px, 8vw);; /* Ajuste la taille du texte */
+  font-weight: 600;
+  text-align: left; /* Aligne le texte à gauche */
+}
+</style>
